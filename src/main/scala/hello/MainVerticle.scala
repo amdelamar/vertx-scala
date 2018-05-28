68ec3132987dd53cb80afbdb932fa5792b0bd743
amdelamar <amdelamar@protonmail.com>
2018-05-27 20:53:45 -0400

    Refractor to companion object

--- src/main/scala/hello/MainVerticle.scala
+++ src/main/scala/hello/MainVerticle.scala
@@ -2,9 +2,21 @@ package hello
 
 import io.vertx.lang.scala.ScalaVerticle
 import scala.reflect.api.materializeTypeTag
+import io.vertx.scala.core.Vertx
+
+object MainVerticle {
+  
+  /** Main method deploys the verticle */
+  def main(args: Array[String]): Unit = {
+    val vertx = Vertx.vertx
+    vertx.deployVerticle(ScalaVerticle.nameForVerticle[hello.MainVerticle])
+  }
+  
+}
 
 class MainVerticle extends ScalaVerticle {
 
+  /** Start the verticle */
   override def start(): Unit = {
     println("Starting")
     vertx
@@ -13,7 +25,7 @@ class MainVerticle extends ScalaVerticle {
         .putHeader("content-type", "text/plain")
         .end("Hello from Scala + Vert.x"))
       .listen(8080)
-     println("Started on port",8080)
+     println("Started on port "+8080)
   }
   
   override def stop(): Unit = {
